340b48efad7b58a10fc7167ee0b9a9d8e663b3ca
it63070179 <73689108+it63070179@users.noreply.github.com>
2022-11-09 19:12:32 +0700

    Delete expressbackend directory

--- expressbackend/routes/web.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import MybillController from '../controllers/mybillController.js';
-const router = express.Router();
-
-router.get('/mybill', MybillController.getAllDoc)
-
-export default router
