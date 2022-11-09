248d72ea7df501b72b5117ff764998e12e96af2b
Ingkawat 63070189 <73688553+Ingkawat@users.noreply.github.com>
2022-11-09 19:42:59 +0700

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,6 +2,18 @@ const express = require("express");
 const User = require('../models/user')
 router = express.Router();
 
+router.post('/signup', async (req,res) => {
+    const user_name  = req.body.user_name
+    const password  = req.body.password
+    const name = req.body.name
+    const email  = req.body.email
+    const phonenumber = req.body.phonenumber
+    const user = new User({user_name , password, name, email, phonenumber});
+    await user.save();
+    
+    res.send("You made a post request")
+})
+
 router.post("/login", async (req, res) => {
     const username  = req.body.user_name
     const password  = req.body.password
@@ -9,12 +21,13 @@ router.post("/login", async (req, res) => {
 
     if(user[0] != undefined){
         if (user[0].password == password){
-            res.json(user[0])
+            // res.json(user[0])
+            res.json("Login success")
         }else{
-            res.json("incorrect password ")
+            res.json("Incorrect username or password.")
         }
     }else{
-        res.json("incorrect username")
+        res.json("Incorrect username or password.")
     }
     
   })
