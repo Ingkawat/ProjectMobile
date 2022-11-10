ffb55456136ec6dd97db2cf0851a1b61db88c56b
Ingkawat 63070189 <73688553+Ingkawat@users.noreply.github.com>
2022-11-11 00:59:19 +0700

    Update MyNavigator.js

--- navigator/MyNavigator.js
+++ navigator/MyNavigator.js
@@ -1,140 +1,30 @@
-import React from "react";
-import { Text, View, TouchableOpacity } from "react-native";
+import React, {useContext, useEffect} from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
-import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-import LoginScreen from "../screens/LoginScreen";
-import RegisterScreen from "../screens/RegisterScreen";
-import SeegroupScreen from "../screens/SeegroupScreen";
-import ProfileScreen from "../screens/ProfileScreen";
-import ForgotPassScreen from "../screens/ForgotPassScreen";
-import BillUserScreen from "../screens/BillUserScreen";
-import AttachevidenceScreen from "../screens/AttachevidenceScreen";
-import PayMoneyScreen from "../screens/PaymoneyScreen";
-import PayalreadyScreen from "../screens/PayalreadyScreen";
-import CreateBillScreen from "../screens/CreateBillScreen";
-import { ScreenStackHeaderCenterView } from "react-native-screens";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Context as AuthContext } from '../context/AuthContext';
+import Login from "../screens/LoginScreen";
+import CreateBill from "../screens/CreateBillScreen";
+import Register from "../screens/RegisterScreen";
+import ForgotPass from "../screens/ForgotPassScreen"
 
 const Stack = createNativeStackNavigator();
-const Tab = createMaterialTopTabNavigator();
-
-function MyTabBar({ state, descriptors, navigation }) {
-  return (
-    <View style={{ flexDirection: "row" }}>
-      {state.routes.map((route, index) => {
-        const { options } = descriptors[route.key];
-        const label =
-          options.tabBarLabel !== undefined
-            ? options.tabBarLabel
-            : options.title !== undefined
-            ? options.title
-            : route.name;
-
-        const isFocused = state.index === index;
-        const onPress = () => {
-          const event = navigation.emit({
-            type: "tabPress",
-            target: route.key,
-          });
-
-          if (!isFocused && !event.defaultPrevented) {
-            navigation.navigate(route.name);
-          }
-        };
-
-        const onLongPress = () => {
-          navigation.emit({
-            type: "tabLongPress",
-            target: route.key,
-          });
-        };
-
-        return (
-          <TouchableOpacity
-            accessibilityRole="button"
-            accessibilityState={isFocused ? { selected: true } : {}}
-            accessibilityLabel={options.tabBarAccessibilityLabel}
-            testID={options.tabBarTestID}
-            onPress={onPress}
-            // onLongPress={onLongPress}
-            style={{
-              flex: 1,
-              backgroundColor: isFocused ? "#FFD47C" : "#FFEBD0",
-              padding: 20,
-            }}
-          >
-            <Text
-              style={{ textAlign: "center", fontSize: 14, fontWeight: "bold" }}
-            >
-              {label}
-            </Text>
-          </TouchableOpacity>
-        );
-      })}
-    </View>
-  );
-}
-
-function TabNavigator() {
-  return (
-    <Tab.Navigator>
-      <Tab.Navigator
-        tabBar={(props) => <MyTabBar {...props} />}
-        screenOptions={{
-          swipeEnabled: false,
-        }}
-      >
-        <Tab.Screen
-          name={"notpaid"}
-          component={SeegroupScreen}
-          options={{ tabBarActiveTintColor: "#000" }}
-        ></Tab.Screen>
-        <Tab.Screen
-          name={"paidAlready"}
-          component={PayalreadyScreen}
-          options={{ tabBarActiveTintColor: "#000" }}
-        ></Tab.Screen>
-      </Tab.Navigator>
-    </Tab.Navigator>
-  );
-}
-
-function LoginNavigator() {
-  return (
-    <Stack.Navigator
-      screenOptions={{
-        headerShown: true,
-        headerTintColor: "white",
-        headerTitleAlign: "center",
-        headerStyle: {
-          backgroundColor: "#5A6199",
-        },
-        headerTitleStyle: {
-          fontWeight: "bold",
-        },
-      }}
-    >
-      <Stack.Screen
-        name="login"
-        component={LoginScreen}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen name="register" component={RegisterScreen} />
-      <Stack.Screen name="forgotpass" component={ForgotPassScreen} />
-      <Stack.Screen
-        name="createBill"
-        component={CreateBillScreen}
-        options={{ headerShown: false }}
-      />
-    </Stack.Navigator>
-  );
-}
+const Tab = createBottomTabNavigator();
 
 export default function MyNavigator() {
+  const {state, tryLocalSingin} = useContext(AuthContext); 
   return (
     <NavigationContainer>
-      <LoginNavigator />
+      {state.username == null ? (
+      <Stack.Navigator screenOptions={{headerShown:true}}>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="ForgotPass" component={ForgotPass} />
+      </Stack.Navigator>
+      ):(<Stack.Navigator screenOptions={{headerShown:false}}>
+        <Stack.Screen name="CreateBill" component={CreateBill} />
+      </Stack.Navigator>)}
     </NavigationContainer>
+
   );
 }
-
