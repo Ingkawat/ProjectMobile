472e4df0332fa167602292e81591fa4afaf337b7
Ingkawat 63070189 <73688553+Ingkawat@users.noreply.github.com>
2022-11-11 00:54:52 +0700

    Update RegisterScreen.js

--- Screens/RegisterScreen.js
+++ Screens/RegisterScreen.js
@@ -1,244 +1,241 @@
-import React , {useState} from "react";
-import {StyleSheet, Text, View, TextInput, TouchableOpacity} from "react-native";
-import { useRef } from "react";
-import { Avatar } from "react-native-paper";
-import * as ImagePicker from 'expo-image-picker';
+import React, { useState, useContext } from "react";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  Alert,
+} from "react-native";
+import * as Animatable from "react-native-animatable";
+import * as ImagePicker from "expo-image-picker";
 
-function RegisterScreen ({navigation}) {
-    const ref_textinput2 = useRef();
-    const ref_textinput3 = useRef();
-    const ref_textinput4 = useRef();
-    const ref_textinput5 = useRef();
-    const ref_textinput6 = useRef();
+import { Context as AuthContext } from "../context/AuthContext";
+import { Context as ValidationContext } from "../context/ValidationContext";
+
+const RegisterScreen = () => {
+  const { state, register } = useContext(AuthContext);
+  const [user_name, setUser_name] = useState();
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmpassword, setConfirmpassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [image, setImage] = useState();
+  const {
+    validate_Username,
+    validate_Password,
+    validate_Email,
+    validate_Name,
+    validate_Phonenumber,
+  } = useContext(ValidationContext);
+
+  const addimage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
     
-    const [image, setImage] = useState(null);
-
-    const pickImage = async () => {
-      let result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.Images,
-        allowsEditing: true,
-        quality: 1,
-      });
-
-      if (!result.cancelled) {
-        setImage(result.uri);
-      }
-    };
-
-    const [userInfo, setUserInfo] = useState({
-      name: '',
-      username: '',
-      password: '',
-      confirmpassword: '',
-      email: '',
-      phonenum: '',
-
-    })
-
-    const isValidObjField = (obj) => {
-      return Object.values(obj).every(value => value.trim())
-    }
+   
+    setImage(result.assets[0].uri);
     
-    const updateError = (error, stateUpdater) =>{
-      stateUpdater(error);
-      setTimeout(() => {
-        stateUpdater('')
-      }, 2000);
-    }
+  };
+
+  let state1 = useContext(ValidationContext).state;
+
+  const [conpass, setConpass] = useState("");
+
+  const onSubmit_Register = () => {
     
-    const {name, username, password, confirmpassword, email, phonenum} = userInfo
-    const [errorReqAll, setErrorReqAll] = useState('')
-    const [checkValidName, setCheckValidName] = useState(true);
-    const [checkValidUsername, setCheckValidUsername] = useState(true);
-    const [checkValidPassword, setCheckValidPassword] = useState(true);
-    const [checkValidConfirmPassword, setCheckValidConfirmPassword] = useState(true);
-    const [checkValidEmail, setCheckValidEmail] = useState(true);
-    const [checkValidPhonenum, setCheckValidPhonenum] = useState(true);
-
-    const handleOnChangeText = (value, yourname) => {
-      setUserInfo({...userInfo, [yourname]: value});
-    }
-
-    const reqAllField = () => {
-      if(!isValidObjField(userInfo)) return updateError('Request all fields!', setErrorReqAll)
-
-    }
-
-    const validateName = (name) => {
-      if(!name.trim() || name.length < 4){
-        return false
+    if (password != confirmpassword) {
+      setConpass("password not match");
+    } else if (
+      state1.errorName === "" &&
+      state1.errorEmail === "" &&
+      state1.errorPassword === "" &&
+      state1.errorPhonenumber === ""
+    ) {
+      if(user_name != '' && name != '' && password != '' && email != '' && phone != ''){
+        register({ user_name, name, password, phone, email, image });
+        ButtonAlert();
+      } else{
+        console.log("not pass")
       }
-      return true
+      
+      
+    } else {
+      console.log("password not match");
     }
+  };
 
-    const validateUsername = (username) => {
-      if(!username.trim() || username.length < 5){
-        return false
-      }
-      return true
-    }
-    
-    const validatePassword = (password) => {
-      if(!password.trim() || password.length < 6){
-        return false
-      }
-      return true
-    }
+  const ButtonAlert = () =>
+    Alert.alert(
+      "Alert Title",
+      "Register is successful",
+      [
+        { text: "Go back to login", onPress: () => { navigation.navigate("Login")} }
+      ]
+    );
 
-    const validateConfirmPassword = (confirmpassword) => {
-      if(password !== confirmpassword){
-        return false
-      }
-      return true
-    }
-
-    const validateEmail = (email) => {
-      let txt = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-      if (txt.test(email)){
-        return true
-      }
-      return false
-    }
-
-    const validatePhonenum = (number) =>{
-      if(!number.trim() || number.length < 10){
-        return false
-      }
-      return true
-    }
 
   return (
-    <View style={styles.container} navigation={navigation}>
-        <View style={{alignItems:'center'}}>
-          <TouchableOpacity onPress={pickImage}>
-            <Avatar.Image
-              size={70}
-              source={{ uri: image }}
-            />
-            <Text style={styles.btnTextAddphoto}>Add Photo</Text>
-          </TouchableOpacity>
-        </View>
-        <TextInput returnKeyType='next' autoCorrect={false} style={styles.textinput} value={name} placeholder="Enter Your Name" 
-        onSubmitEditing={() => ref_textinput2.current.focus()}
-        onChangeText={(value) => {
-          handleOnChangeText(value, 'name')
-          const IsValid = validateName(value);
-          IsValid? setCheckValidName(true) : setCheckValidName(false);}}/>
-
-        <Text style={styles.textfail}>{checkValidName? '' : 'Name must be more than 3 characters!'}</Text>
-        {errorReqAll ? <Text style={styles.textfail2}>{errorReqAll}</Text>:null}
-
-        <TextInput returnKeyType='next' autoCorrect={false} style={styles.textinput} value={username} placeholder="Enter Username" 
-        onSubmitEditing={() => ref_textinput3.current.focus()} ref={ref_textinput2}
-        onChangeText={(value) => {
-          handleOnChangeText(value, 'username')
-          const IsValid = validateUsername(value);
-          IsValid? setCheckValidUsername(true) : setCheckValidUsername(false);}}/>
-
-        <Text style={styles.textfail}>{checkValidUsername? '' : 'Username must be more than 6 characters!'}</Text>
-        {errorReqAll ? <Text style={styles.textfail2}>{errorReqAll}</Text>:null}
-        
-        <TextInput returnKeyType='next' autoCorrect={false} secureTextEntry={true}
-        style={styles.textinput} value={password} placeholder="Enter Password" 
-        onSubmitEditing={() => ref_textinput4.current.focus()} ref={ref_textinput3}
-        onChangeText={(value) => {
-          handleOnChangeText(value, 'password')
-          const IsValid = validatePassword(value);
-          IsValid? setCheckValidPassword(true) : setCheckValidPassword(false);}}/>
-
-        <Text style={styles.textfail}>{checkValidPassword? '' : 'Password is less than 6 characters!'}</Text>
-        {errorReqAll ? <Text style={styles.textfail2}>{errorReqAll}</Text>:null}
-        
-        <TextInput returnKeyType='next' autoCorrect={false} secureTextEntry={true}
-        style={styles.textinput} value={confirmpassword} placeholder="Confirm Password"
-        onSubmitEditing={() => ref_textinput5.current.focus()} ref={ref_textinput4}
-        onChangeText={(value) => {
-          handleOnChangeText(value, 'confirmpassword')
-          const IsValid = validateConfirmPassword(value);
-          IsValid? setCheckValidConfirmPassword(true) : setCheckValidConfirmPassword(false);}}/>
-
-        <Text style={styles.textfail}>{checkValidConfirmPassword? '' : 'Password does not match!'}</Text>
-        {errorReqAll ? <Text style={styles.textfail2}>{errorReqAll}</Text>:null}
-        
-        <TextInput keyboardType="email-address" autoCorrect={false} returnKeyType='next' style={styles.textinput} 
-        value={email} placeholder="Enter Email"  onSubmitEditing={() => ref_textinput6.current.focus()} ref={ref_textinput5}
-        onChangeText={(value) => {
-          handleOnChangeText(value, 'email')
-          const IsValid = validateEmail(value);
-          IsValid? setCheckValidEmail(true) : setCheckValidEmail(false);}}/>
-        
-        <Text style={styles.textfail}>{checkValidEmail? '' : 'Invalid Email!'}</Text>
-        {errorReqAll ? <Text style={styles.textfail2}>{errorReqAll}</Text>:null}
-        
-        <TextInput keyboardType="phone-pad" autoCorrect={false} style={styles.textinput} maxLength={10} returnKeyType='done' 
-        value={phonenum} placeholder="Enter Phonenumber" ref={ref_textinput6}
-        onChangeText={(value) => {
-          handleOnChangeText(value, 'phonenum')
-          const IsValid = validatePhonenum(value);
-          IsValid? setCheckValidPhonenum(true) : setCheckValidPhonenum(false);}}/>
-
-        <Text style={styles.textfail}>{checkValidPhonenum? '' : 'Phone number must be equal to 10 characters!'}</Text>
-        {errorReqAll ? <Text style={styles.textfail2}>{errorReqAll}</Text>:null}
-
-        <TouchableOpacity onPress={reqAllField} style={styles.btn}>
-            <Text style={styles.btntext}>Next</Text>
+    <View style={styles.screen}>
+      <View style={styles.circle}>
+        <TouchableOpacity onPress={() => addimage()}>
+          <Image
+            source={require("../assets/add_picture.png")}
+            style={styles.imgAdd}
+          ></Image>
         </TouchableOpacity>
+      </View>
+      <Text style={{ color: "#F06B6D", fontWeight: "bold", marginTop: 10 }}>
+        Add Photo
+      </Text>
+      <TextInput
+        style={styles.txtInput}
+        placeholder="Enter Your Name"
+        value={name}
+        onChangeText={(value) => {
+          setName(value), validate_Name(value);
+        }}
+      ></TextInput>
+      {state1.errorName ? (
+        <Animatable.View animation="fadeInLeft" duration={500}>
+          <Text style={{ color: "red" }}>{state1.errorName}</Text>
+        </Animatable.View>
+      ) : null}
+      <TextInput
+        style={styles.txtInput}
+        placeholder="Enter User Name"
+        value={user_name}
+        onChangeText={(value) => {
+          setUser_name(value), validate_Username(value);
+        }}
+      ></TextInput>
+      {state1.errorUsername ? (
+        <Animatable.View animation="fadeInLeft" duration={500}>
+          <Text style={{ color: "red" }}>{state1.errorUsername}</Text>
+        </Animatable.View>
+      ) : null}
+      <TextInput
+        style={styles.txtInput}
+        placeholder="Enter Password"
+        value={password}
+        onChangeText={(value) => {
+          setPassword(value), validate_Password(value);
+        }}
+      ></TextInput>
+      {state1.errorPassword ? (
+        <Animatable.View
+          animation="fadeInLeft"
+          duration={500}
+          style={{ flexWrap: "nowrap", marginLeft: "5%", marginRight: "2%" }}
+        >
+          <Text style={{ color: "red", left: 25 }}>{state1.errorPassword}</Text>
+        </Animatable.View>
+      ) : null}
+      <TextInput
+        style={styles.txtInput}
+        placeholder="Confirm Password"
+        value={confirmpassword}
+        onChangeText={(value) => {
+          setConfirmpassword(value);
+        }}
+      ></TextInput>
+      {conpass ? (
+        <Animatable.View animation="fadeInLeft" duration={500}>
+          <Text style={{ color: "red" }}>{conpass}</Text>
+        </Animatable.View>
+        
+      ) : null}
+      <TextInput
+        style={styles.txtInput}
+        placeholder="Enter Email"
+        value={email}
+        onChangeText={(value) => {
+          setEmail(value), validate_Email(value);
+        }}
+      ></TextInput>
+      {state1.errorEmail ? (
+        <Animatable.View animation="fadeInLeft" duration={500}>
+          <Text style={{ color: "red" }}>{state1.errorEmail}</Text>
+        </Animatable.View>
+      ) : null}
+      <TextInput
+        style={styles.txtInput}
+        placeholder="Enter Phonenumber"
+        value = {phone} onChangeText={(value) => {setPhone(value), validate_Phonenumber(value)}}
+      ></TextInput>
+      {state1.errorPhonenumber ?
+          <Animatable.View animation="fadeInLeft" duration={500}>
+           <Text style={{color:"red"}}>{state1.errorPhonenumber}</Text>
+          </Animatable.View> :null }
+      <TouchableOpacity style={styles.btn} onPress={()=> onSubmit_Register()}>
+        <Text style={{ color: "#FFF8EF", fontWeight: "bold" }}>
+          Create Account
+        </Text>
+      </TouchableOpacity>
+      {state.Message == 'register is success' ? (
+        <Animatable.View
+          animation="fadeInLeft"
+          duration={500}
+          style={{ flexWrap: "nowrap" }}
+        >
+          <Text style={{ color: "green" }}>{state.Message}</Text>
+        </Animatable.View>
+      ) :(
+        <Animatable.View
+          animation="fadeInLeft"
+          duration={500}
+          style={{ flexWrap: "nowrap" }}
+        >
+          <Text style={{ color: "red" }}>{state.Message}</Text>
+        </Animatable.View>
+      )}
+
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
-    backgroundColor: '#FFF8EF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FFF8EF",
   },
 
-  textinput: {
-    height: 30,
-    width: 250,
-    alignSelf: 'center',
-    marginTop: 5,
-    marginBottom: 10,
-    color: 'black',
-    borderBottomColor: 'black',
+  circle: {
+    backgroundColor: "#FCC046",
+    borderRadius: 100,
+    padding: 30,
+  },
+
+  imgAdd: {
+    width: 130,
+    height: 130,
+    marginLeft: -5,
+  },
+
+  txtInput: {
+    height: 25,
+    width: "60%",
+    marginTop: 25,
+    paddingHorizontal: 10,
     borderBottomWidth: 1,
   },
 
   btn: {
-    alignSelf: 'center',
-    width: 150,
-    padding: 10,
-    backgroundColor: '#F06B6D',
-    marginTop: 20,
-    borderRadius: 10,
+    backgroundColor: "#F06B6D",
+    paddingHorizontal: 50,
+    paddingVertical: 10,
+    borderRadius: 5,
+    marginBottom: 20,
+    marginTop: 25,
   },
-
-  btntext: {
-    color: 'white',
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-
-  btnTextAddphoto: {
-    color: '#F06B6D',
-    fontWeight: 'bold',
-    textAlign: 'center',
-
-  },
-
-  textfail: {
-    fontSize: 12,
-    color: 'red',
-    marginLeft: 65,
-    marginTop: -13,
-  },
-
-  textfail2: {
-    fontSize: 12,
-    color: 'red',
-    marginLeft: 80,
-    marginTop: 5,
-  }
 });
 
 export default RegisterScreen;
