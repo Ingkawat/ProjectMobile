72b685d48da2ecb57d041239280c458a42ca2b32
Apibun <73689795+Apibun@users.noreply.github.com>
2022-11-18 20:11:53 +0700

    Add files via upload

---
+++ Screens/ContactScreen.js
@@ -0,0 +1,132 @@
+import axios from "axios";
+import React, { useEffect, useContext, useState } from "react";
+import {
+  FlatList,
+  Image,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import Constants from "expo-constants";
+import { Context as AuthContext } from "../context/AuthContext";
+import FriendList from "../components/FriendList";
+
+const ContactScreen = () => {
+  const { state } = useContext(AuthContext);
+  const [nameUser, setNameUser] = useState("");
+  const [friendNameForAdd, setfriendNameForAdd] = useState("");
+  const [seeFriendRequest, setseeFriendRequest] = useState(null);
+  const [friendNameForAccept, setfriendNameForAccept] = useState("");
+  const [allFriend, setallFriend] = useState(null);
+
+  useEffect(() => {
+    axios
+      .post(
+        `http://${Constants.expoConfig.extra.apiUrl}:3000/user/${state.username}`
+      )
+      .then(async (res) => {
+        const Friend = res.data.friends;
+        setNameUser(res.data.name);
+        const findRequestFriend = Friend.filter(
+          ({ status }) => status === "Pending"
+        );
+        setseeFriendRequest(findRequestFriend);
+        setallFriend(Friend);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
+  const AddFriend = () => {
+    axios
+      .post(
+        `http://${Constants.expoConfig.extra.apiUrl}:3000/sendFriendRequest/${nameUser}`,
+        { FriendName: friendNameForAdd }
+      )
+      .then(async (res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const accpetFriend = (prop) => {
+    setfriendNameForAccept(prop);
+    console.log(prop);
+    console.log(friendNameForAccept);
+    axios
+      .post(
+        `http://${Constants.expoConfig.extra.apiUrl}:3000/acceptFriendRequest/${friendNameForAccept}/${nameUser}`
+      )
+      .then(async (res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const renderFriendRequest = (itemData) => {
+    return (
+      <FriendList
+        name={itemData.item.name}
+        status={itemData.item.status}
+        acceptFriendFunc={(prop) => accpetFriend(prop)}
+      />
+    );
+  };
+
+  return (
+    <View style={styles.screen}>
+      <TextInput
+        style={styles.txtInput}
+        value={friendNameForAdd}
+        onChangeText={(value) => setfriendNameForAdd(value)}
+      ></TextInput>
+      <TouchableOpacity style={styles.btn} onPress={() => AddFriend()}>
+        <Text style={{ color: "#FFF8EF", fontWeight: "bold" }}>
+          เพิ่มเพื่อน
+        </Text>
+      </TouchableOpacity>
+      <Text style={{ fontSize: 18, fontWeight: "bold", marginTop: "5%" }}>
+        Contact
+      </Text>
+      <FlatList data={seeFriendRequest} renderItem={renderFriendRequest} />
+      <FlatList data={allFriend} renderItem={renderFriendRequest} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    height: "100%",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    backgroundColor: "#FFF8EF",
+    paddingTop: "10%",
+  },
+
+  txtInput: {
+    height: 25,
+    width: "50%",
+    marginTop: 10,
+    paddingHorizontal: 10,
+    borderBottomWidth: 1,
+  },
+
+  btn: {
+    backgroundColor: "#525EBC",
+    paddingHorizontal: 30,
+    paddingVertical: 10,
+    borderRadius: 5,
+    marginTop: 20,
+  },
+});
+
+export default ContactScreen;
