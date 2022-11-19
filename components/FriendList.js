63116ed86d9ce373641a31a10e84d79600393f63
Apibun <73689795+Apibun@users.noreply.github.com>
2022-11-19 13:46:19 +0700

    Update FriendList.js
    update function and condition

--- components/FriendList.js
+++ components/FriendList.js
@@ -1,62 +1,103 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { TouchableOpacity, View, Text, StyleSheet, Image } from "react-native";
 
 const FriendList = (props) => {
   const status = props.status;
-  const onPressButton = () => {
+  const sentByYourSelf = props.sentByYourSelf;
+
+  const acceptFriend = () => {
     props.acceptFriendFunc(props.name);
   };
 
-  return (
-    <View
-      style={{
-        flexDirection: "row",
-        marginTop: "5%",
-        justifyContent: "center",
-      }}
-    >
-      <Image
-        source={require("../assets/userprofileicon.png")}
-        style={styles.userProfile}
-      ></Image>
-      <Text
-        style={{
-          marginLeft: "5%",
-          fontSize: 14,
-          marginTop: "2%",
-          fontWeight: "bold",
-        }}
-      >
-        {props.name}
-      </Text>
-      {status === "Pending" ? (
-        <View style={{ flexDirection: "row", marginLeft: "30%" }}>
-          <TouchableOpacity
-            style={[styles.btn, { backgroundColor: "#77CEC6" }]}
-            onPress={() => onPressButton()}
+  const unFriend = () => {
+    props.unFriendFunc(props.name);
+  };
+
+  const renderFriendList = () => {
+    if (status === "Pending" && sentByYourSelf === false) {
+      return (
+        <View
+          style={{
+            flexDirection: "row",
+            marginTop: "5%",
+            justifyContent: "center",
+          }}
+        >
+          <Image
+            source={require("../assets/userprofileicon.png")}
+            style={styles.userProfile}
+          ></Image>
+          <Text
+            style={{
+              marginLeft: "5%",
+              fontSize: 14,
+              marginTop: "2%",
+              fontWeight: "bold",
+            }}
           >
-            <Text style={{ color: "#FFF8EF", fontWeight: "bold" }}>Accept</Text>
-          </TouchableOpacity>
+            {props.name}
+          </Text>
+          <View style={{ flexDirection: "row", marginLeft: "30%" }}>
+            <TouchableOpacity
+              style={[styles.btn, { backgroundColor: "#77CEC6" }]}
+              onPress={() => acceptFriend()}
+            >
+              <Text style={{ color: "#FFF8EF", fontWeight: "bold" }}>
+                Accept
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={[styles.btn, { backgroundColor: "#F06B6D" }]}
+              onPress={() => unFriend()}
+            >
+              <Text style={{ color: "#FFF8EF", fontWeight: "bold" }}>
+                Unfriend
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      );
+    } else if (status === "Accepted") {
+      return (
+        <View
+          style={{
+            flexDirection: "row",
+            marginTop: "5%",
+            justifyContent: "center",
+          }}
+        >
+          <Image
+            source={require("../assets/userprofileicon.png")}
+            style={styles.userProfile}
+          ></Image>
+          <Text
+            style={{
+              marginLeft: "5%",
+              fontSize: 14,
+              marginTop: "2%",
+              fontWeight: "bold",
+            }}
+          >
+            {props.name}
+          </Text>
           <TouchableOpacity
-            style={[styles.btn, { backgroundColor: "#F06B6D" }]}
+            style={[
+              styles.btn,
+              { backgroundColor: "#F06B6D", marginLeft: "40%" },
+            ]}
           >
             <Text style={{ color: "#FFF8EF", fontWeight: "bold" }}>
               Unfriend
             </Text>
           </TouchableOpacity>
         </View>
-      ) : (
-        <TouchableOpacity
-          style={[
-            styles.btn,
-            { backgroundColor: "#F06B6D", marginLeft: "40%" },
-          ]}
-        >
-          <Text style={{ color: "#FFF8EF", fontWeight: "bold" }}>Unfriend</Text>
-        </TouchableOpacity>
-      )}
-    </View>
-  );
+      );
+    } else {
+      return null;
+    }
+  };
+
+  return renderFriendList();
 };
 
 const styles = StyleSheet.create({
