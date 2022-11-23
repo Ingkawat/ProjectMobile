4a3dfc06f95d9a6d7f656f10b31604b615b0a4be
it63070179 <73689108+it63070179@users.noreply.github.com>
2022-11-24 03:13:57 +0700

    Update ForgotPassScreen.js

--- Screens/ForgotPassScreen.js
+++ Screens/ForgotPassScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import {
   Image,
   StyleSheet,
@@ -7,37 +7,155 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
+import { Context as AuthContext } from "../context/AuthContext";
+import { Alert } from "react-native";
 
-const ForgotPassScreen = () => {
+const ForgotPassScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [checkValidEmail, setCheckValidEmail] = useState(false);
+  const [checkValidPassword, setCheckValidPassword] = useState(false);
+  const { forgotpass, repassword } = useContext(AuthContext);
+  const [visible, setVisible] = useState(false);
+  const [token, setResetCode] = useState("");
+
+  const handleCheckEmail = (text) => {
+    let txt = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    setEmail(text);
+
+    if (txt.test(text)) {
+      setCheckValidEmail(false);
+    } else {
+      setCheckValidEmail(true);
+    }
+  };
+
+  const handleCheckPassword = (text) => {
+    let txt =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!#@_=;:()*"':{}[%\$%\^&\*])(?=.{8,})/;
+    setPassword(text);
+
+    if (txt.test(text)) {
+      setCheckValidPassword(false);
+    } else {
+      setCheckValidPassword(true);
+    }
+  };
+
+  const ButtonAlert = () =>
+    Alert.alert(
+      "Alert",
+      "Please check your inbox of mail",
+      [
+        {
+          text: "OK",
+        },
+      ],
+    );
+
+  const ButtonAlert2 = () =>
+    Alert.alert(
+      "Alert",
+      "Error sending email. Try again!",
+      [
+        {
+          text: "OK",
+        },
+      ],
+      setEmail("")
+    );
+
+  const ButtonAlert3 = () =>
+    Alert.alert("Alert", "Now. You can login with new password", [
+      {
+        text: "OK",
+      },
+    ]);
+
+  const ButtonAlert4 = () =>
+    Alert.alert("Alert", "New Password or Password Reset Code Invalid", [
+      {
+        text: "OK",
+      },
+    ]);
+
+  const submit_forgotpass = () => {
+    if (!checkValidEmail && email != "") {
+      forgotpass({ email });
+      ButtonAlert();
+      setVisible(true);
+    } else {
+      ButtonAlert2();
+    }
+  };
+
+  const handlePasswordReset = () => {
+    let tokenLength = token.length
+    if (!checkValidPassword && tokenLength == 5 && token != "") {
+      repassword({ email, password, token });
+      ButtonAlert3();
+      navigation.navigate('Login');
+    } else {
+      ButtonAlert4();
+    }
+  };
+
   return (
     <View style={styles.screen}>
-      <View style={{ flexDirection: "row", marginBottom: 60 }}>
-        <Image
-          source={require("../assets/password_icon.png")}
-          style={styles.icon}
-        ></Image>
-        <TextInput
-          style={styles.txtInput}
-          placeholder="New password"
-        ></TextInput>
-      </View>
-      <View style={{ flexDirection: "row", marginBottom: 60 }}>
-        <Image
-          source={require("../assets/password_icon.png")}
-          style={styles.icon}
-        ></Image>
-        <TextInput
-          style={styles.txtInput}
-          placeholder="Confirm new password"
-        ></TextInput>
-      </View>
       <View
         style={{ flexDirection: "row", marginBottom: 60, marginRight: -20 }}
       >
-        <TextInput style={styles.txtInput} placeholder="Email"></TextInput>
+        <TextInput
+          style={styles.txtInput}
+          keyboardType="email-address"
+          placeholder="Email"
+          value={email}
+          onChangeText={(text) => handleCheckEmail(text)}
+        ></TextInput>
       </View>
-      <TouchableOpacity style={styles.btn}>
-        <Text style={{ color: "#FFF8EF" }}>Reset Password</Text>
+      {checkValidEmail ? (
+        <Text style={styles.textfail}>Is not e-mail rules.</Text>
+      ) : (
+        <Text style={styles.textfail}></Text>
+      )}
+
+      {visible && (
+        <>
+          <TextInput
+            style={styles.txtInput}
+            placeholder="New Password"
+            value={password}
+            secureTextEntry={true}
+            onChangeText={(value) => {
+              handleCheckPassword(value);
+            }}
+          ></TextInput>
+          {checkValidPassword ? (
+            <Text style={styles.textfail2}>
+              Password must have a large, small, special, {"\n"} and must have 8
+              characters.
+            </Text>
+          ) : (
+            <Text style={styles.textfail2}></Text>
+          )}
+          <TextInput
+            style={styles.txtInput2}
+            placeholder="Password Reset Code"
+            maxLength={5}
+            onChangeText={(value) => {
+              setResetCode(value);
+            }}
+          ></TextInput>
+        </>
+      )}
+
+      <TouchableOpacity
+        style={styles.btn}
+        onPress={visible ? handlePasswordReset : submit_forgotpass}
+      >
+        <Text style={{ color: "#FFF8EF" }}>
+          {visible ? "Reset Password" : "Request Reset Code"}
+        </Text>
       </TouchableOpacity>
     </View>
   );
@@ -61,7 +179,15 @@ const styles = StyleSheet.create({
   txtInput: {
     height: 25,
     width: "60%",
-    marginTop: 10,
+    marginTop: 20,
+    paddingHorizontal: 10,
+    borderBottomWidth: 1,
+  },
+
+  txtInput2: {
+    height: 25,
+    width: "60%",
+    marginTop: 30,
     paddingHorizontal: 10,
     borderBottomWidth: 1,
   },
@@ -72,6 +198,21 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     marginTop: 50,
     backgroundColor: "#525EBC",
+  },
+
+  textfail: {
+    fontSize: 12,
+    color: "red",
+    fontWeight: "bold",
+    marginLeft: 150,
+    marginTop: -50,
+  },
+
+  textfail2: {
+    fontSize: 12,
+    color: "red",
+    fontWeight: "bold",
+    marginLeft: 10,
   },
 });
 
